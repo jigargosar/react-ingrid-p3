1adf3cf990eb1548e70835d4897b9b37fb72aacb
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-26 17:50:42 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useLayoutEffect, useReducer, useRef } from 'react'
 import { initialState, rootReducer } from './rootReducer'
-import { selectLineAction, selectNextAction, selectPrevAction } from './actions'
+import {
+  selectLineAction,
+  selectNextAction,
+  selectPrevAction,
+} from './actions'
 import { useHotKeyDispatcher } from './hooks/useHotKey'
 import { getCached, useCacheEffect } from './hooks/useCacheEffect'
 
 function Line({ line, isSelected, dispatch }) {
-
-  let sc = `${isSelected ? 'bg-blue white' : ''}`
+  const sc = `${isSelected ? 'bg-blue white' : ''}`
   const ref = useRef()
 
   useLayoutEffect(() => {
@@ -16,21 +19,32 @@ function Line({ line, isSelected, dispatch }) {
     }
   }, [isSelected])
 
-  return <div ref={ref} key={line.id} className={`lh-copy ph3 br2 ${sc}`}
-              tabIndex={0}
-              onFocus={() => selectLineAction(line, dispatch)}
-  >
-    {line.title}
-  </div>
+  return (
+    <div
+      ref={ref}
+      key={line.id}
+      className={`lh-copy ph3 br2 ${sc}`}
+      tabIndex={0}
+      onFocus={() => selectLineAction(line, dispatch)}
+    >
+      {line.title}
+    </div>
+  )
 }
 
 function App() {
-
   const cacheKey = 'react-ingrid-p3'
-  const [state, dispatch] = useReducer(rootReducer, null, () => getCached(cacheKey) || initialState())
+  const [state, dispatch] = useReducer(
+    rootReducer,
+    null,
+    () => getCached(cacheKey) || initialState(),
+  )
   useCacheEffect(cacheKey, state)
 
-  const currentHotKeyMap = () => ({ up: selectPrevAction, down: selectNextAction })
+  const currentHotKeyMap = () => ({
+    up: selectPrevAction,
+    down: selectNextAction,
+  })
 
   useHotKeyDispatcher(currentHotKeyMap, dispatch)
 
@@ -40,10 +54,15 @@ function App() {
         {state.lines.map(line => {
           const isSelected = line.id === state.selectedId
 
-          return <Line key={line.id} line={line} isSelected={isSelected} dispatch={dispatch}
-          />
+          return (
+            <Line
+              key={line.id}
+              line={line}
+              isSelected={isSelected}
+              dispatch={dispatch}
+            />
+          )
         })}
-
       </div>
     </div>
   )
