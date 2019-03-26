d808164c569f7de0ad471205cd820dff12b10661
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-26 17:28:04 +0530

    useReducer

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,15 @@
 export const SELECT_LINE = 'sl'
+export const SELECT_NEXT_LINE = 'sNext'
+export const SELECT_PREV_LINE = 'sPrev'
 
 export function selectLineAction(line, dispatch) {
   dispatch({ type: SELECT_LINE, line })
 }
 
 export function selectNextAction(dispatch) {
-  dispatch({ type: 'sNext' })
+  dispatch({ type: SELECT_NEXT_LINE })
 }
 
 export function selectPrevAction(dispatch) {
-  dispatch({ type: 'sPrev' })
+  dispatch({ type: SELECT_PREV_LINE })
 }
