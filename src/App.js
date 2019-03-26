6c839a21e869738a15bb80148d1a4155988e750a
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-26 16:45:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import nanoid from 'nanoid'
 import faker from 'faker'
 import { times } from 'ramda'
@@ -7,26 +7,42 @@ function newLine() {
   return { id: `id_${nanoid()}`, title: faker.name.lastName() }
 }
 
+function initialState() {
+  let lines = times(newLine, 10)
+  return {
+    lines,
+    selectedId: lines[0].id,
+  }
+}
+
+function cachedState() {
+  const jsonString = localStorage.getItem('react-ingrid-p3')
+  if (jsonString) {
+    return JSON.parse(jsonString)
+  }
+  return null
+}
+
 function App() {
-  const [state, setState] = useState(()=>{
-    let lines = times(newLine, 10)
-    return {
-      lines,
-      selectedId:lines[0].id
-    }
-  })
+
+  const [state, setState] = useState(() => initialState())
+
+  useEffect(() => {
+    localStorage.setItem('react-ingrid-p3', JSON.stringify(state))
+  }, [state])
+
   return (
     <div className="min-vh-100 pv3 ph2">
       <div className="code">
         {state.lines.map(line => {
           const isSelected = line.id === state.selectedId
 
-          let cc = `${isSelected ? "bg-blue white" : ""}`
+          let cc = `${isSelected ? 'bg-blue white' : ''}`
 
           return <div key={line.id} className={`lh-copy ph3 br2 ${cc}`}
                       tabIndex={0}
-                      onFocus={()=>setState(s=>{
-                        return {...s, selectedId: line.id}
+                      onFocus={() => setState(s => {
+                        return { ...s, selectedId: line.id }
                       })}
           >
             {line.title}
@@ -38,4 +54,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
